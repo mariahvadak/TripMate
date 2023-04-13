82a43e0e3ff88f7eeb1bca7cd6dd3becbcb91830
mariahjv <mariah.vadak@ou.edu>
2023-04-13 11:10:18 -0500

    Create to-do list pages

--- app/views.py
+++ app/views.py
@@ -5,3 +5,17 @@ views = Blueprint('views',__name__)
 @views.route('/')
 def home_page():
     return render_template("home.html")
+
+@views.route('/to-do-lists/')
+def to_do_lists():
+    # Fetch the list of to-do lists from a database or file system
+    to_do_lists = ['To-Do List 1', 'To-Do List 2', 'To-Do List 3', 'To-Do List 4', 'To-Do List 5', 'To-Do List 6', 
+                   'To-Do List 7','To-Do List 8', 'To-Do List 9', 'To-Do List 10',]
+
+    # Pass the list of to-do lists to the to-do list template
+    return render_template("to-do-list.html", to_do_lists=to_do_lists)
+
+@views.route('/todo-doc/')
+def to_do_doc():
+    
+    return render_template("todo_doc.html")
