07e8f6b291077dd5caa8518317f678553672e4d7
mariahjv <mariah.vadak@ou.edu>
2023-04-13 16:14:30 -0500

    Fix todo_doc

--- app/views.py
+++ app/views.py
@@ -10,7 +10,9 @@ def home_page():
 def to_do_lists():
     # Fetch the list of to-do lists from a database or file system
     to_do_lists = ['To-Do List 1', 'To-Do List 2', 'To-Do List 3', 'To-Do List 4', 'To-Do List 5', 'To-Do List 6', 
-                   'To-Do List 7','To-Do List 8', 'To-Do List 9', 'To-Do List 10',]
+                   'To-Do List 7','To-Do List 8', 'To-Do List 9', 'To-Do List 10','To-Do List 10','To-Do List 10',
+                   'To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10',
+                   'To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10',]
 
     # Pass the list of to-do lists to the to-do list template
     return render_template("to-do-list.html", to_do_lists=to_do_lists)
