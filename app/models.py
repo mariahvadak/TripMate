630892914223c1c9ad7283ef3af601d67bff6385
mariahjv <mariah.vadak@ou.edu>
2023-04-15 15:05:35 -0500

    Redo todo list doc

--- app/models.py
+++ app/models.py
@@ -8,4 +8,20 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String())
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
+
+class ToDoList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('to_do_lists', lazy=True))
+    tasks = db.relationship('Task', backref='to_do_list', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+    to_do_list_id = db.Column(db.Integer, db.ForeignKey('to_do_list.id'))
     
--- app/views.py
+++ app/views.py
@@ -1,23 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import current_user, login_required
+from .models import ToDoList, Task
+from . import db
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
 
 @views.route('/')
-def home_page():
-    return render_template("home.html")
+@login_required
+def home():
+    return render_template('home.html')
 
-@views.route('/to-do-lists/')
+@views.route('/to-do-lists/', methods=['GET', 'POST'])
 def to_do_lists():
-    # Fetch the list of to-do lists from a database or file system
-    to_do_lists = ['To-Do List 1', 'To-Do List 2', 'To-Do List 3', 'To-Do List 4', 'To-Do List 5', 'To-Do List 6', 
-                   'To-Do List 7','To-Do List 8', 'To-Do List 9', 'To-Do List 10','To-Do List 10','To-Do List 10',
-                   'To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10',
-                   'To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10','To-Do List 10',]
+    if request.method == 'POST':
+        # Get the title of the new to-do list from the form
+        title = request.form.get('title')
+        # Create a new to-do list object
+        new_list = ToDoList(title=title, user=current_user)
+        # Add the to-do list to the database
+        db.session.add(new_list)
+        db.session.commit()
+        return redirect(url_for('views.to_do_lists'))
 
-    # Pass the list of to-do lists to the to-do list template
-    return render_template("to-do-list.html", to_do_lists=to_do_lists)
+    # If the user is trying to edit a to-do list, get the ID of the list and render the to-do list template
+    list_id = request.args.get('list_id')
+    if list_id:
+        to_do_list = ToDoList.query.filter_by(id=list_id, user=current_user).first()
+        if to_do_list:
+            # If the list exists, get all its tasks and render the to-do list template
+            tasks = Task.query.filter_by(to_do_list_id=list_id).all()
+            return render_template('to-do-list.html', to_do_list=to_do_list, tasks=tasks)
+
+    # If the user is not trying to edit a to-do list, render the dashboard template with all the to-do lists
+    to_do_lists = ToDoList.query.filter_by(user=current_user).all()
+    return render_template('to-do-list.html', to_do_lists=to_do_lists)
 
 @views.route('/todo-doc/')
-def to_do_doc():
-    
-    return render_template("todo_doc.html")
+def doc():
+    return render_template('todo_doc.html')
