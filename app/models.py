572c568dfdcb0e227fefb14ae05eb9d8800e3c6f
mariahjv <mariah.vadak@ou.edu>
2023-04-18 23:25:40 -0500

    Todo list mostly working

--- app/models.py
+++ app/models.py
@@ -9,19 +9,12 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
 
-class ToDoList(db.Model):
+class TodoList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+    title = db.Column(db.String(100))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', backref=db.backref('to_do_lists', lazy=True))
-    tasks = db.relationship('Task', backref='to_do_list', lazy=True)
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(200))
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
-    to_do_list_id = db.Column(db.Integer, db.ForeignKey('to_do_list.id'))
+    def __repr__(self):
+        return '<TodoList %r>' % self.title
     
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask import jsonify
 from flask_login import current_user, login_required
-from .models import ToDoList, Task
+from .models import TodoList
 from . import db
 
 views = Blueprint('views', __name__)
@@ -10,30 +11,43 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html')
 
-@views.route('/to-do-lists/', methods=['GET', 'POST'])
-def to_do_lists():
+@views.route('/todolists/', methods=['GET', 'POST'])
+@login_required
+def todolist():
+    # Query all the to-do lists from the database
+    to_do_lists = TodoList.query.all()
+
     if request.method == 'POST':
-        # Get the title of the new to-do list from the form
-        title = request.form.get('title')
-        # Create a new to-do list object
-        new_list = ToDoList(title=title, user=current_user)
-        # Add the to-do list to the database
-        db.session.add(new_list)
+        # Get the title of the new to-do list from the form data
+        title = request.json.get('title')
+
+        # Create a new to-do list object and add it to the database
+        new_todo_list = TodoList(title=title, user_id=current_user.id)
+        db.session.add(new_todo_list)
         db.session.commit()
-        return redirect(url_for('views.to_do_lists'))
 
-    # If the user is trying to edit a to-do list, get the ID of the list and render the to-do list template
-    list_id = request.args.get('list_id')
-    if list_id:
-        to_do_list = ToDoList.query.filter_by(id=list_id, user=current_user).first()
-        if to_do_list:
-            # If the list exists, get all its tasks and render the to-do list template
-            tasks = Task.query.filter_by(to_do_list_id=list_id).all()
-            return render_template('to-do-list.html', to_do_list=to_do_list, tasks=tasks)
+        # Return the ID of the newly created todo list as a JSON response
+        return jsonify(id=new_todo_list.id)
 
-    # If the user is not trying to edit a to-do list, render the dashboard template with all the to-do lists
-    to_do_lists = ToDoList.query.filter_by(user=current_user).all()
-    return render_template('to-do-list.html', to_do_lists=to_do_lists)
+    # Render the to-do list template with the list of to-do lists and links to each individual to-do list
+    return render_template('to-do-list.html', to_do_lists=to_do_lists, individual_links=True)
+
+@views.route('/todolists/<int:todo_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_todo_list(todo_id):
+    # Query the todo list with the specified ID from the database
+    todo_list = TodoList.query.get(todo_id)
+
+    if request.method == 'POST':
+        # Update the title of the todo list
+        todo_list.title = request.form['title']
+        db.session.commit()
+
+        # Redirect the user to the todo list dashboard
+        return redirect(url_for('views.todolist'))
+
+    # Render the edit todo list template with the specified todo list and its items
+    return render_template('todo_doc.html', todo_list=todo_list)
 
 @views.route('/todo-doc/')
 def doc():
